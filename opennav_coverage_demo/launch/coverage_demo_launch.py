b7403356646847fcae66cd53da0b2cebb77914a6
Adi Vardi <adi.vardi@enway.ai>
2024-11-05 12:27:26 +0000

    [tmp] trying to solve linking issues

--- opennav_coverage_demo/launch/coverage_demo_launch.py
+++ opennav_coverage_demo/launch/coverage_demo_launch.py
@@ -13,34 +13,35 @@
 # limitations under the License.
 
 import os
+import tempfile
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, OpaqueFunction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.event_handlers import OnShutdown
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
 
 
 def generate_launch_description():
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     coverage_demo_dir = get_package_share_directory('opennav_coverage_demo')
+    sim_dir = get_package_share_directory('nav2_minimal_tb3_sim')
 
     world = os.path.join(coverage_demo_dir, 'blank.world')
+    # world = os.path.join(sim_dir, 'worlds', 'tb3_sandbox.sdf.xacro')
     param_file_path = os.path.join(coverage_demo_dir, 'demo_params.yaml')
-    sdf = os.path.join(nav2_bringup_dir, 'worlds', 'waffle.model')
-
-    # start the simulation
-    start_gazebo_server_cmd = ExecuteProcess(
-        cmd=['gzserver', '-s', 'libgazebo_ros_init.so',
-             '-s', 'libgazebo_ros_factory.so', world],
-        cwd=[coverage_demo_dir], output='screen')
+    robot_sdf = os.path.join(sim_dir, 'urdf', 'gz_waffle.sdf.xacro')
 
     # start_gazebo_client_cmd = ExecuteProcess(
     #     cmd=['gzclient'],
     #     cwd=[coverage_demo_dir], output='screen')
 
-    urdf = os.path.join(nav2_bringup_dir, 'urdf', 'turtlebot3_waffle.urdf')
+    urdf = os.path.join(sim_dir, 'urdf', 'turtlebot3_waffle.urdf')
     with open(urdf, 'r') as infp:
         robot_description = infp.read()
 
@@ -52,15 +53,64 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True,
                      'robot_description': robot_description}])
 
-    start_gazebo_spawner_cmd = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+    # start the simulation
+    world_sdf = tempfile.mktemp(prefix='nav2_', suffix='.sdf')
+    world_sdf_xacro = ExecuteProcess(
+        cmd=['xacro', '-o', world_sdf, ['headless:=', 'False'], world])
+    gazebo_server = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', '-s', world_sdf],
         output='screen',
-        arguments=[
-            '-entity', 'tb3',
-            '-file', sdf,
-            '-x', '5.0', '-y', '5.0', '-z', '0.10',
-            '-R', '0.0', '-P', '0.0', '-Y', '0.0'])
+        # condition=IfCondition(True)
+    )
+
+    remove_temp_sdf_file = RegisterEventHandler(event_handler=OnShutdown(
+        on_shutdown=[
+            OpaqueFunction(function=lambda _: os.remove(world_sdf))
+        ]))
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'),
+                         'launch',
+                         'gz_sim.launch.py')
+        ),
+        # condition=IfCondition(PythonExpression(
+        #     [True, ' and not ', False])),
+        launch_arguments={'gz_args': ['-v4 -g ']}.items(),
+    )
+
+    # start_gazebo_spawner_cmd = Node(
+    #     package='gazebo_ros',
+    #     executable='spawn_entity.py',
+    #     output='screen',
+    #     arguments=[
+    #         '-entity', 'tb3',
+    #         '-file', sdf,
+    #         '-x', '5.0', '-y', '5.0', '-z', '0.10',
+    #         '-R', '0.0', '-P', '0.0', '-Y', '0.0'])
+
+    pose = {
+        'x': LaunchConfiguration('x_pose', default='-2.00'),
+        'y': LaunchConfiguration('y_pose', default='-0.50'),
+        'z': LaunchConfiguration('z_pose', default='0.01'),
+        'R': LaunchConfiguration('roll', default='0.00'),
+        'P': LaunchConfiguration('pitch', default='0.00'),
+        'Y': LaunchConfiguration('yaw', default='0.00'),
+    }
+
+    gz_robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sim_dir, 'launch', 'spawn_tb3.launch.py')),
+        launch_arguments={'namespace': '',
+                          'use_sim_time': True,
+                          'robot_name': 'turtlebot3_waffle',
+                          'robot_sdf': robot_sdf,
+                          'x_pose': pose['x'],
+                          'y_pose': pose['y'],
+                          'z_pose': pose['z'],
+                          'roll': pose['R'],
+                          'pitch': pose['P'],
+                          'yaw': pose['Y']}.items())
 
     # start the visualization
     rviz_config = os.path.join(coverage_demo_dir, 'opennav_coverage_demo.rviz')
@@ -91,10 +141,15 @@ def generate_launch_description():
         output='screen')
 
     ld = LaunchDescription()
-    ld.add_action(start_gazebo_server_cmd)
+    ld.add_action(world_sdf_xacro)
+    ld.add_action(remove_temp_sdf_file)
+    # ld.add_action(gz_robot)
+    ld.add_action(gazebo_server)
+    ld.add_action(gazebo_client)
+    # ld.add_action(start_gazebo_server_cmd)
     # ld.add_action(start_gazebo_client_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
-    ld.add_action(start_gazebo_spawner_cmd)
+    # ld.add_action(start_gazebo_spawner_cmd)
     ld.add_action(rviz_cmd)
     ld.add_action(bringup_cmd)
     ld.add_action(fake_localization_cmd)
